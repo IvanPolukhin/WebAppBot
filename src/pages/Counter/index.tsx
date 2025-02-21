5ce7043b98b55f967141ad12a708652d8eb7abb6
IvanPolukhin <ivan.poluhin.200407@gmail.com>
2025-02-21 21:59:33 +0200

    feat: add counter

--- src/pages/Counter/index.tsx
+++ src/pages/Counter/index.tsx
@@ -1,10 +1,34 @@
-import NavigationButtons from 'src/components/NavigationButtons'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from 'src/store/store'
+import { decrement, increment, reset } from 'src/features/Сounter/counterSlice'
 
 const Counter = () => {
+  const dispatch = useDispatch()
+  const count = useSelector((state: RootState) => state.counter.count)
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold">Counter Page</h1>
-      <NavigationButtons />
+      <h1 className="text-4xl font-bold">Counter: {count}</h1>
+      <div className="mt-4 flex gap-4">
+        <button
+          onClick={() => dispatch(increment())}
+          className="px-4 py-2 bg-green-500 rounded"
+        >
+          +
+        </button>
+        <button
+          onClick={() => dispatch(decrement())}
+          className="px-4 py-2 bg-red-500 rounded"
+        >
+          -
+        </button>
+        <button
+          onClick={() => dispatch(reset())}
+          className="px-4 py-2 bg-gray-500 rounded"
+        >
+          Reset
+        </button>
+      </div>
     </div>
   )
 }
